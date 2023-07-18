fdb9da1d2ce262110fb8cbf8f04300432df7bf33
maringrgatovic <56473997+maringrgatovic@users.noreply.github.com>
2023-07-18 15:09:37 +0200

    Delete scripts directory

--- scripts/mobile.js
+++
@@ -1,7 +0,0 @@
-var front = document.getElementById('card_face_front');
-
-front.innerHTML = '<img src="images/Pozivnica_internet_mobitel_naprid.jpg">'
-
-var front = document.getElementById('card_face_back');
-
-front.innerHTML = '<img src="images/Pozivnica_internet_mobitel_nazad.jpg">'
